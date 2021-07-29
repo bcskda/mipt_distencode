2805ed0fbd9b864e333e90f4e0b2cf3eac0f2bbc
bcskda <demulkup@ya.ru>
2021-07-30 00:26:18 +0300

    Refactor manager (+db support)

--- mipt_distencode/config.py
+++ mipt_distencode/config.py
@@ -3,3 +3,4 @@ import os
 
 class Config:
     identity = os.environ['DISTENC_IDENTITY']
+    db = os.environ['DISTENC_DB']
--- mipt_distencode/manager/manager.py
+++ mipt_distencode/manager/manager.py
@@ -3,9 +3,11 @@ import logging
 
 import grpc
 
-from mipt_distencode.jobs_pb2 import JobId, MLTJob
-from mipt_distencode.mgmt_messages_pb2 import WorkerSelfAnnouncement, WorkerState
+from mipt_distencode import jobs_pb2, mgmt_messages_pb2
 from mipt_distencode.manager import manager_pb2_grpc
+from mipt_distencode.manager.db_models import (
+    MLTJobHandle, MLTJobState, Session, WorkerRecord, WorkerState
+)
 from mipt_distencode.pb_common import PeerIdentityMixin
 
 
@@ -13,65 +15,65 @@ class ManagerServicer(manager_pb2_grpc.ManagerServicer, PeerIdentityMixin):
     """Not thread-safe, use only with single-threaded RPC server"""
     def __init__(self):
         super().__init__()
-        self.next_job_id = 1
         self.workers = collections.deque()
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
 
     def WorkerAnnounce(self, announcement, context):
         peer_id = self.identify_peer(context)
+        state = WorkerState.from_proto(announcement.newState)
         if peer_id != announcement.hostname:
             message = 'Peer {0} identified as {1} is not {2}'.format(
                 context.peer(), peer_id, announcement.hostname)
             context.abort(grpc.StatusCode.INVALID_ARGUMENT, message)
 
-        if announcement.newState == WorkerState.ACTIVE:
+        if state == WorkerState.ACTIVE:
             return self.add_worker(announcement, context)
-        elif announcement.newState == WorkerState.STOPPING:
+        elif state == WorkerState.STOPPING:
             return self.remove_worker(announcement, context)
         else:
             message = f'Unknown WorkerState: {announcement.newState}'
             self.logger.error(message)
             context.abort(grpc.StatusCode.INVALID_ARGUMENT, message)
 
-    def PostMLTJob(self, job, context):
+    def PostMLTJob(self, proto, context):
         peer_id = self.identify_peer(context)
-        job_id = self._register_job(job, context)
-        worker = self._rotate_workers()
-        job_str = str(job).replace('\n', ' ')
-        self.logger.info(f'Accepted job: id={job_id}, params={job_str}, assigned worker {worker}')
-        return JobId(id=job_id)
-
-    def add_worker(self, announcement, context):
-        worker = announcement.hostname
-        if worker in self.workers:
-            message = f'Duplicate worker: {worker}, peer={context.peer()}'
-            self.logger.warning(message)
-        else:
-            self.workers.append(worker)
-            self.logger.info('Registered worker: %s', worker)
-        return announcement
-    
-    def remove_worker(self, announcement, context):
-        worker = announcement.hostname
-        if worker in self.workers:
-            self.workers.remove(worker)
-            self.logger.info('Unregistered worker: %s', worker)
-            return announcement
-        else:
-            message = f'Worker not found: {worker}, peer={context.peer()}'
-            self.logger.error(message)
-            context.abort(grpc.StatusCode.NOT_FOUND, message)
-
-    def _register_job(self, job, context):
-        if job.HasField('id'):
+        if proto.HasField('id'):
             context.abort(
                 grpc.StatusCode.INVALID_ARGUMENT, f'Id must not be provided')
-        this_id = self.next_job_id
-        self.next_job_id += 1
-        return this_id
+        with Session() as session:
+            job_handle = MLTJobHandle.new_from_proto(proto, session)
+            worker = self._choose_worker()
+            self.logger.info(f'Accepted job: {job_handle}')
+            return jobs_pb2.JobId(id=job_handle.id)
 
-    def _rotate_workers(self):
+    def add_worker(self, proto, context):
+        with Session() as session:
+            worker_record = WorkerRecord.lookup_from_proto(proto, session)
+            if worker_record is None:
+                worker_record = WorkerRecord.new_from_proto(proto, session)
+            else:
+                message = f'Duplicate worker: {worker_record.hostname}, peer={context.peer()}'
+                self.logger.warning(message)
+            self.workers.append(worker_record.hostname)
+            self.logger.info('Successfully registered worker: %s', worker_record.hostname)
+        return proto
+
+    def remove_worker(self, proto, context):
+        with Session() as session:
+            worker_record = WorkerRecord.lookup_from_proto(proto, session)
+            if worker_record is None:
+                message = f'Worker not found: {worker_record.hostname}, peer={context.peer()}'
+                self.logger.error(message)
+                context.abort(grpc.StatusCode.NOT_FOUND, message)
+            host = worker_record.hostname
+            session.delete(worker_record)
+            session.commit()
+            self.workers.remove(host)
+            self.logger.info('Successfully unregistered worker: %s', host)
+        return proto
+
+    def _choose_worker(self):
         chosen = self.workers.popleft()
         self.workers.append(chosen)
         return chosen
